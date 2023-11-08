d6bdbcb29feffa3c3dfaf3cb01736d44b81113ba
Tobias Fischer <info@tobiasfischer.info>
2023-11-09 08:07:57 +1000

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -11,6 +11,7 @@ cmake ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DBUILD_SHARED_LIBS=ON ^
+    -DBUILD_VERSION=%PKG_VERSION% ^
     %SRC_DIR%
 if errorlevel 1 exit 1
 
